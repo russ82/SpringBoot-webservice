3256d537dc483bbdf0d375006a53c445e4c21170
russ82 <jinhs1995@gmail.com>
2021-01-19 01:56:36 +0900

    CRUD v1

---
+++ src/main/java/com/jojoidu/book/springboot/web/dto/PostsResponseDto.java
@@ -0,0 +1,21 @@
+package com.jojoidu.book.springboot.web.dto;
+
+import com.jojoidu.book.springboot.domain.posts.Posts;
+import lombok.Getter;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Getter
+public class PostsResponseDto {
+
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity){
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+}
