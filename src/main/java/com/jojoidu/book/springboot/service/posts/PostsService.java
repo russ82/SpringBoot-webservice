cd9667251a78448a373c9000f59a3eaf7d3f8c72
russ82 <jinhs1995@gmail.com>
2021-01-21 01:47:45 +0900

    oauth v1

--- src/main/java/com/jojoidu/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/jojoidu/book/springboot/service/posts/PostsService.java
@@ -2,13 +2,16 @@ package com.jojoidu.book.springboot.service.posts;
 
 import com.jojoidu.book.springboot.domain.posts.Posts;
 import com.jojoidu.book.springboot.domain.posts.PostsRepository;
+import com.jojoidu.book.springboot.web.dto.PostsListResponseDto;
 import com.jojoidu.book.springboot.web.dto.PostsResponseDto;
 import com.jojoidu.book.springboot.web.dto.PostsSaveRequestDto;
 import com.jojoidu.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import javax.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -22,15 +25,28 @@ public class PostsService {
 
     @Transactional
     public Long update(Long id, PostsUpdateRequestDto requesDto){
-        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id ="+ id));
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id ="+ id));
         posts.update(requesDto.getTitle(), requesDto.getContent());
 
         return id;
     }
 
     public PostsResponseDto findById(Long id){
-        Posts entity = postsRepository.findById(id).orElseThrow(()-> new IllegalArgumentException("해당 게시글이 없습니다. id=" +id));
+        Posts entity = postsRepository.findById(id).orElseThrow(()-> new IllegalArgumentException("해당 사용자가 없습니다. id=" +id));
 
         return new PostsResponseDto(entity);
+    }
+
+    // import 버전 꼭맞춰주기
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new).collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id){
+        Posts posts = postsRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당 사용자가 없습니다. id" + id));
+
+        postsRepository.delete(posts);
     }
 }
